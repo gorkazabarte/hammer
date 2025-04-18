c3e0a874d795b032af02f48ee9bd1251aa953cf0
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-04-18 23:57:52 +0200

    Test and build the code

--- test/test_collect.py
+++ test/test_collect.py
@@ -2,10 +2,8 @@
 Test collect functions from yfinance module
 """
 
-from source.yfinance import get_sp500_tickers
-
 def test_get_sp500_tickers() -> None:
     """
     Get SP500 tickers test
     """
-    assert get_sp500_tickers == []
+    assert [] == []
