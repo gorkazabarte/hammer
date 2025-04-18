e5945cc16312f4c8784f2dddd5a17245c1127d9a
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-04-18 23:40:25 +0200

    HAM-3 Create first version of the Hammer server with FastAPI

---
+++ source/app.py
@@ -0,0 +1,14 @@
+"""Welcome to Hammer backend application created with FastAPI"""
+
+from json import load
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def get_root():
+    """This is the main get method"""
+
+    with open('./company/amd.json', 'r', encoding='utf-8') as file:
+        data = load(file)
+    return {"data": data}
--- source/main.py
+++
@@ -1,5 +0,0 @@
-from yfinance.collect import get_sp500_tickers
-
-if __name__ == '__main__': 
-    print(get_sp500_tickers())
-
