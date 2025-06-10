c65f2c46d9bc96030433b26091b3407b2f77f28e
gorkazabarte <gorkazabartemoreno@gmail.com>
2025-06-10 22:22:52 +0100

    HAM-3 Fix Pylint issues of CI

--- source/app.py
+++ source/app.py
@@ -1,13 +1,14 @@
+"""Welcome to Hammer backend application created with FastAPI"""
+
 from json import load
-from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def get_root():
+    """This is the main get method"""
 
-    with open('./company/amd.json', 'r') as file:
+    with open('./company/amd.json', 'r', enconding='utf8') as file:
         data = load(file)
     return {"data": data}
-
